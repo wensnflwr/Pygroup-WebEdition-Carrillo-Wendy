05df0855d09fb2e61ecd5dfbecae379d625fbb79
repl.it user <replituser@example.com>
2020-11-10 06:12:09 +0000

    flask

--- store/app/__init__.py
+++ store/app/__init__.py
@@ -1,15 +1,25 @@
-from flask import Flask
+from flask import Flask, Response
+from products.models import prod
 
 app = Flask(__name__) 
 
-@app.route(('/<string:name>'), methods = ['GET']) 
-def index(name): 
-  if one == "pygroup": 
-    return Response("ERROR! No se puede usar el nombre pygroup", status = 400) 
-  return  Response("Felicitaciones! Trabajo exitoso {}".format(name), status = 200) 
+@app.route('/')
+def start():
+  return 'Hello there !'
+
+@app.route(('/<name>/').lower(), methods = ['GET']) 
+def user(name): 
+  if name == "pygroup": 
+    return Response("ERROR! No se puede usar el nombre pygroup", status = 400)
+    
+  else: 
+    return Response("Felicitaciones! Trabajo exitoso {}".format(name), status = 200)
+    
 
 #from store.app.products import views 
 #from store.app.products.views import products 
+app.register_blueprint(prod)
   
 if __name__ == "__main__": 
-  app.run(bug = True)
+  app.run('0.0.0.0', port = 8080)
+
--- store/app/products/views.py
+++ store/app/products/views.py
@@ -1,1 +1,17 @@
- 
+#Flask permite usar plantillas para el contenido dinámico de páginas web.
+
+#Para usar este se puede usar el método "render_template()". Lo único que se debe hacer es proveer el nombre de la plantilla y las variables que se vayan a pasar al mecanismo de plantillas
+
+#Ejemplo:
+
+from flask import render_template
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+#Enlaces de busqueda:
+
+#https://riptutorial.com/es/flask/example/5303/uso-de-render-template
+#https://flask.palletsprojects.com/en/1.1.x/quickstart/
